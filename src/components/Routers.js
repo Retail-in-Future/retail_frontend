8e90dd715dd18d27d2f17a8f2b854e24e2af8d2f
Chuan.Peng <cpeng@thoughtworks.com>
2017-05-19 14:16:34 +0800

    Add change event to react router.

--- src/components/Routers.js
+++ src/components/Routers.js
@@ -7,9 +7,13 @@ import Main from '../pages/main/';
 import Login from '../pages/login/';
 import Logout from '../pages/logout/';
 
+const handleChange = (...args) => {
+    console.log(args);
+};
+
 const Routers = () => (
     <Router history={hashHistory}>
-        <Route path="/" component={Frame}>
+        <Route path="/" component={Frame} onChange={handleChange}>
             <Route path="main" component={Main} />
             <Route path="login" component={Login} />
             <Route path="logout" component={Logout} />
