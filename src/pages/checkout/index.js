fc6de778154897b661073e56e42e0b91745b854b
Linesh <linesh.simpcity@gmail.com>
2017-08-03 22:52:18 +0800

    [#6][Linesh] ♻️Refactor: change pure function into a React class

--- src/pages/checkout/index.js
+++ src/pages/checkout/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/forbid-prop-types, react/require-default-props */
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { Layout } from 'antd';
@@ -11,59 +11,33 @@ import ProductSummary from './ProductSummary';
 
 const { Header, Content } = Layout;
 
-const Checkout = ({ products = [
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 },
-  { name: 'product', quantity: 1, price: 200.00 }
-] }) => {
-    return (
-        <Layout>
-            <Header style={{ position: 'fixed', width: '100%' }}>
-                { _.isEmpty(products) ? '结账指引' : '商品清单' }
-            </Header>
-            <Content style={{ marginTop: 64 }}>
-                {
-                    _.isEmpty(products) ?
+class Checkout extends Component {
+    componentDidMount() {
+
+    }
+
+    render() {
+        const products = this.props.products || [
+          { name: '茅台王子酒53度（酱香型）', price: 200.00, quantity: 2 },
+          { name: '茅台王子酒53度（酱香型）', price: 200.00, quantity: 2 },
+          { name: '茅台王子酒53度（酱香型）', price: 200.00, quantity: 2 }
+        ];
+
+        return (
+            <Layout>
+                <Header style={{ position: 'fixed', width: '100%' }}>
+                    { _.isEmpty(products) ? '结账指引' : '商品清单' }
+                </Header>
+                <Content style={{ marginTop: 64 }}>
+                    { _.isEmpty(products) ?
                         <UserGuide /> :
                         <ProductSummary products={products} />
-                }
-            </Content>
-        </Layout>
-    );
-};
+                    }
+                </Content>
+            </Layout>
+        );
+    }
+}
 
 Checkout.propTypes = {
     products: PropTypes.array
