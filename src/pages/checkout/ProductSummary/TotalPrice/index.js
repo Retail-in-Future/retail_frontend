715ed399d0370fc5b62d73c51649e0494e7147a7
Linesh <linesh.simpcity@gmail.com>
2017-08-04 13:25:50 +0800

    [#6][Linesh] 🎨Style: styling

--- src/pages/checkout/ProductSummary/TotalPrice/index.js
+++ src/pages/checkout/ProductSummary/TotalPrice/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { Col, Row } from 'antd';
+
+import styles from './TotalPrice.scss';
+
 const TotalPrice = ({ totalQuantity, totalPrice }) => (
-    <div>
-        <p>数量 { totalQuantity }</p>
-        <p>金额 { totalPrice }</p>
-    </div>
+    <Row className={styles.container} type="flex" justify="center" align="middle">
+        <Col span={12}>
+            <div className={styles.sumLabel}>合计：</div>
+            <div className={styles.quantity}>数量 { totalQuantity }</div>
+            <div className={styles.totalPrice}>金额 { totalPrice }</div>
+        </Col>
+        <Col span={12}>
+            请将二维码对准扫描器
+        </Col>
+    </Row>
 );
 
 TotalPrice.propTypes = {
